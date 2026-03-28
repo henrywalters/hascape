9effffc3018cf94a5ddb412b9b4993357ee9c0e4
Henry Walters <henrywalters20@gmail.com>
2026-03-28 12:44:06 -0400

    Fix backend docker

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "hascape",
-    password: "hascape",
-    database: "hascape",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "5432"),
+    username: process.env.DB_USER || "hascape",
+    password: process.env.DB_PASSWORD || "hascape",
+    database: process.env.DB_NAME || "hascape",
     synchronize: true,
     logging: false,
     entities: ["src/entity/**/*.ts"],
