26b3b37f76cf41c8eec0f4699895e3a1edbba934
Henry Walters <henrywalters20@gmail.com>
2026-03-29 17:03:00 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /root/hascape
+docker compose build
+
+cd /root/hagameonline
+docker compose build
+
+cd /root/hascape
+docker-compose down --remove-orphans
+docker-compose up -d
+
+cd /root/hagameonline
+docker-compose down --remove-orphans
+docker-compose up -d
